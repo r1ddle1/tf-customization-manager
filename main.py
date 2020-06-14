4818729ccd1008e707d074aa9bf9854056fab585
r1ddle <46493085+r1ddle1@users.noreply.github.com>
2020-06-14 22:13:28 +0300

    Remove useless 'Color' class

--- main.py
+++ main.py
@@ -10,17 +10,6 @@ import pages
 from sys import argv
 
 
-class Color(QWidget):
-
-    def __init__(self, color, *args, **kwargs):
-        super(Color, self).__init__(*args, **kwargs)
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
-
-
 # Subclass QMainWindow to customise your application's main window
 class MainWindow(QMainWindow):
     def __init__(self, tf_path, *args, **kwargs):
